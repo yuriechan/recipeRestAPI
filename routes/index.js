93d0d9588cb6f3541a193ed8e4915748aeb97d56
Yurie <cutecupcakeandcute@gmail.com>
2020-07-13 12:47:11 +0900

    add endpoint to get all posts

--- routes/index.js
+++ routes/index.js
@@ -3,9 +3,19 @@ const express = require('express')
 const bodyParser = require('body-parser')
 const urlencodedParser = bodyParser.urlencoded({ extended: false })
 const router = express.Router()
+const { db, mysql } = require('../mySqlConfig')
 
-router.get('/', function (req, res, next) {
+router.get('/', (req, res, next) => {
     res.send("hello world")
+})
+
+router.get('/recipes', (req, res, next) => {
+    let sql = 'SELECT * FROM recipes'
+    let query = db.query(sql, (err, results) => {
+        if (err) return console.log(err)
+        console.log(results)
+        res.send(results)
+    })
 })
 
 module.exports = router 
